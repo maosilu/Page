b960b5d771cbce207ea49b28b8dd657df48516a2
Ice Guo <maosilu.ice@gmail.com>
2017-12-07 10:29:15 +0800

    SQL语句 分页的显示： 第一步：连接数据库，按分页取数据 第二步：添加了上一页，下一页 第三步：添加了首页，尾页 第四步：添加显示的页数，规定页数显示5页，其他用省略号（...）代替

---
+++ page.php
@@ -0,0 +1,98 @@
+<html>
+<head>
+	<meta http-equiv="Content-type" content="text/html;charset=utf-8">
+</head>
+<body>
+<?php
+/** 1.传入页码*/
+$page = $_GET['page'];
+
+/** 2.取数据*/
+$pageSize = 10; //每页显示的条数
+$pre = $page-1; //上一页
+$next = $page+1; //下一页
+$show_page = 5; //显示的页码数
+$pageoffset = ($show_page-1)/2; //页码的偏移量
+
+//连接数据库
+$link = mysqli_connect('localhost', 'root', '123456');
+if(!$link){
+	die('数据库链接失败！');
+}
+mysqli_select_db($link, 'test');
+mysqli_set_charset($link, 'utf8');
+//获取分页数据
+$sql = "SELECT * FROM page LIMIT ".($page-1)*$pageSize.', '.$pageSize;
+$result = mysqli_query($link, $sql);
+echo "<table border=1 cellspacing=0 width=40%>";
+echo "<tr><th>id</th>
+<th>name</th></tr>";
+while($row = mysqli_fetch_assoc($result)){
+	echo "<tr>";
+	echo "<td>{$row['id']}</td>";
+	echo "<td>{$row['name']}</td>";
+	echo "</tr>";
+}
+echo "</table>";
+
+//获取数据总数
+$total_sql = "SELECT * FROM page";
+$total_rows = mysqli_num_rows(mysqli_query($link, $total_sql));
+$total_pages = ceil($total_rows/$pageSize);
+
+mysqli_free_result($result);
+mysqli_close($link);
+
+/** 3.显示数据 + 页码*/
+$start = 1; //初始化开始显示页码
+$end = $total_pages; //初始化结束显示页码
+
+$page_banner = '';
+if($page > 1){
+	$page_banner .= "<a href='".$_SERVER['PHP_SELF']."?page=1'>首页</a>";
+	$page_banner .= "<a href='".$_SERVER['PHP_SELF']."?page={$pre}'><上一页</a>";
+}else{
+	$page_banner .= "<span>首页</span>";
+	$page_banner .= "<span><上一页</span>";
+}
+
+if($total_pages > $show_page){
+	if($page > $pageoffset+1){
+		$page_banner .= '...';
+	}
+
+	if($page > $pageoffset){
+		$start = $page-$pageoffset;
+		$end = $total_pages > $page+$pageoffset ? $page+$pageoffset : $total_pages;
+	}else{
+		$end = $show_page;
+	}
+
+	if($page+$pageoffset > $total_pages){
+		$start = $start-($page+$pageoffset-$total_pages);
+	}
+
+	for($i=$start; $i<=$end; $i++){
+		$page_banner .= "<a href='".$_SERVER['PHP_SELF']."?page={$i}'>{$i}</a>";
+	}
+
+	if($page+$pageoffset < $total_pages){
+		$page_banner .= '...';
+	}
+}
+
+
+if($page < $total_pages){
+	$page_banner .= "<a href='".$_SERVER['PHP_SELF']."?page={$next}'>下一页></a>";
+	$page_banner .= "<a href='".$_SERVER['PHP_SELF']."?page={$total_pages}'>尾页></a>";
+}else{
+	$page_banner .= "<span>下一页></span>";
+	$page_banner .= "<span>尾页</span>";
+}
+
+
+$page_banner .= "共{$total_pages}页，";
+echo $page_banner;
+?>
+</body>
+</html>
